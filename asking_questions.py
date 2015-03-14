948a2cc7f749fad6f58759f62e4b40f91a614c32
steph <stephieoldcorn@gmail.com>
2015-03-14 19:18:37 +0000

    Refactoring

--- asking_questions.py
+++ asking_questions.py
@@ -1,6 +1,4 @@
-print 'How old are you?',
-age = raw_input()
-print 'How tall are you?',
-height = raw_input()
-
-print 'So, you\'re %s and %s tall.' % (age, height)
+age = int(raw_input('How old are you? '))
+height = raw_input('How tall are you? ')
+ 
+print 'So, you\'re %d and %s tall.' % (age, height)
